b732c5a16d32988d7c0916ef99a8470f57e01a90
Elton Lunardi EVERCO <elunardi@everco.com.br>
2024-05-22 17:14:24 -0300

    add language for test

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from flask_login import LoginManager
 from flask_babel import Babel, lazy_gettext as _l
 import logging
 import json
+import os
+import sys
 
 
 db = SQLAlchemy()
@@ -109,6 +111,16 @@ def create_app(config_name="default"):
     def before_request():
         pass
     """
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
     return app
 
